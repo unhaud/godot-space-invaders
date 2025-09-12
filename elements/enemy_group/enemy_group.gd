3215603b1652a815030be71622b3a8c831887c39
unhaud <unhaud@gmail.com>
2025-09-12 19:05:15 +0300

    Enemy group
    -enemies are grouped up
    -enemies are bouncing between edges of screen(bugged)

---
+++ elements/enemy_group/enemy_group.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const ROW_STEP = 10.0
+
+@onready var block_timer := $BlockTimer
+
+var direction :=Vector2.RIGHT
+var speed := 10.0
+
+func _process(delta: float):
+	global_position += direction * speed * delta
+
+func change_direction():
+	if block_timer.time_left > 0:
+		return
+	direction = Vector2.LEFT if direction == Vector2.RIGHT else Vector2.RIGHT
+	
+	global_position.y += ROW_STEP
+	block_timer.start()
